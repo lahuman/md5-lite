371977d5752ffbba53257779c66f395aba9a3a23
Daniel Lim <lahuman@daum.net>
2021-01-17 21:36:20 +0900

    fix : use path resolve

--- index.ts
+++ index.ts
@@ -9,24 +9,25 @@ class Md5Lite {
     }
  
     md5Lite(filePath:string):string {
-        const st = fs.statSync(path.join(__dirname, filePath));
+        const basePath = path.resolve(filePath);
+        const st = fs.statSync(basePath);
 
         // 대용량의 경우만 처리
         if (st.size > (this.BUFFER_SIZE * 2)) {
             let firstBuffer = Buffer.alloc(this.BUFFER_SIZE);
             let lastBuffer = Buffer.alloc(this.BUFFER_SIZE);
-            const res = fs.openSync(path.join(__dirname, filePath), 'r');
+            const res = fs.openSync(basePath, 'r');
             fs.readSync(res, firstBuffer, 0, this.BUFFER_SIZE, 0);
             fs.readSync(res, lastBuffer, 0, this.BUFFER_SIZE, (st.size - this.BUFFER_SIZE));
             return md5( Buffer.concat([firstBuffer, lastBuffer]));
         } else {
-            return md5File.sync(path.join(__dirname, filePath));
+            return md5File.sync(basePath);
         }
     }
 
 }
 
-export function md5Lite(filePath: string): string {
+export default function md5Lite(filePath: string): string {
    return new Md5Lite().md5Lite(filePath);
 }
 
